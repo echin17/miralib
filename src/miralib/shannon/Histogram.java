c578ab582a5d5d36d0f21c917f27b234102dd81c
echin17 <e.chin@ucla.edu>
2015-06-26 17:30:12 -0400

    added files and overloaded calculate function

--- src/miralib/shannon/Histogram.java
+++ src/miralib/shannon/Histogram.java
@@ -13,6 +13,11 @@ public class Histogram {
     return calculate(slice, nbins[0], nbins[1]);
   }
   
+  static public int[][] calculate2DArray(DataSlice2D slice, int binAlgo) {
+	  int[] nbins = BinOptimizer.calculate(slice, binAlgo);
+	  return calculate2DArray(slice, nbins[0], nbins[1]);
+  }
+  
   static public ArrayList<ArrayList<Double>> calculate(DataSlice2D slice, int nbinx, int nbiny) {
     if (nbinx < 2 || nbiny < 2) return null;
     
@@ -37,4 +42,20 @@ public class Histogram {
     
     return hist;
   }  
+  
+  static public int[][] calculate2DArray(DataSlice2D slice, int nbinx, int nbiny) {
+	    if (nbinx < 2 || nbiny < 2) return null;
+	    
+	    float sbinx = 1.0f / nbinx;
+	    float sbiny = 1.0f / nbiny;
+	    int[][] counts = new int[nbinx][nbiny];
+	    
+	    for (Value2D value: slice.values) {
+	      int bx = Numbers.constrain((int)(value.x / sbinx), 0, nbinx - 1);  
+	      int by = Numbers.constrain((int)(value.y / sbiny), 0, nbiny - 1);  
+	      counts[bx][by] += value.w;
+	    }
+	    return counts;
+	  }
+  
 }
--- src/miralib/shannon/Similarity.java
+++ src/miralib/shannon/Similarity.java
@@ -4,8 +4,16 @@ package miralib.shannon;
 
 import org.apache.commons.math3.distribution.GammaDistribution;
 import org.apache.commons.math3.distribution.NormalDistribution;
+import org.apache.commons.math3.stat.correlation.SpearmansCorrelation;
+import org.apache.commons.math3.stat.correlation.PearsonsCorrelation;
+import org.apache.commons.math3.distribution.TDistribution;
+
+import java.util.ArrayList;
+import java.math.RoundingMode;
 import java.util.HashMap;
+
 import miralib.data.DataSlice2D;
+import miralib.data.Value2D;
 import miralib.data.Variable;
 import miralib.math.Numbers;
 import miralib.utils.Project;
@@ -18,6 +26,89 @@ import miralib.utils.Project;
 public class Similarity {
   protected static NormalDistribution normDist = new NormalDistribution();
   protected static HashMap<Double, Double> criticalValues = new HashMap<Double, Double>();
+  
+  static public float calculate(DataSlice2D slice, Project prefs, int depTest, int numTail, int binAlgo, float pvalue) {
+	    //prefs.pvalue() = pvalue;
+	    //int binAlgo = prefs.binAlgo; //it's overloaded so user can quickly compare different algorithms
+
+	    Variable varx = slice.varx;
+	    Variable vary = slice.vary;
+
+	    if (varx.weight() || vary.weight() || (varx.subsample() && vary.subsample())) {
+	      // weight variables are not comparable, or subsample variables between 
+	      // each other
+	      return 0;
+	    } 
+	    
+	    Double area = new Double(1 - pvalue/2);
+	    Double cval = criticalValues.get(area);
+	    if (cval == null) {
+	      cval = normDist.inverseCumulativeProbability(area);      
+	      criticalValues.put(area,  cval);
+	    } 
+	    
+	    int count = slice.values.size();
+	    int[] res = BinOptimizer.calculate(slice, prefs.binAlgo);
+	    int binx = res[0];
+	    int biny = res[1];
+	    
+	    float ixy = MutualInformation.calculate(slice, binx, biny);
+	    boolean indep = false;
+	    
+	    
+	    if (Float.isNaN(ixy) || Float.isInfinite(ixy)) {
+	      indep = true;
+	    } else if (prefs.depTest == DependencyTest.NO_TEST || Numbers.equal(pvalue, 1)) {
+	      indep = ixy <= prefs.threshold;
+	    } else if (depTest == DependencyTest.SURROGATE_GAUSS) {
+	    	indep = surrogateGauss(slice, ixy, binAlgo, prefs.surrCount, cval);                 
+	    } else if (depTest == DependencyTest.SURROGATE_GENERAL) {      
+	      indep = surrogateGeneral(slice, ixy, binAlgo, pvalue);
+	    } else if (depTest == DependencyTest.GAMMA_TEST) {
+	      indep = gammaTest(ixy, binx, biny, count, pvalue);
+	    } else if (depTest == DependencyTest.SPEARMAN_TEST) {
+	    	indep = spearmanTest(slice, pvalue, numTail);
+	    } else if (depTest == DependencyTest.PEARSON_TEST) {
+	    	indep = pearsonTest(slice, pvalue, numTail);
+	    } else if (depTest == DependencyTest.FISHER_TEST) {
+	    	indep = fisherTest(slice, pvalue, binAlgo, numTail);
+	    } 
+	    /*else if (depTest == CHISQUARE_TEST) {
+	    	indep = chisquareTest(ixy,binx,biny,count,pvalue);
+	    }
+	    */
+	    /*
+	    if (indep) {
+	        return 0;
+	      } else {
+	        float hxy = JointEntropy.calculate(slice, binx, biny);      
+	        float w;
+	        if (Numbers.equal(0.0, hxy)) {
+	          w = 0;
+	        } else {
+	          w = Numbers.constrain(ixy / hxy, 0, 1);
+	          if (Float.isNaN(w)) w = 0;
+	        }      
+	        return w;
+	      }
+	    */
+	    if (indep) {
+	      return 0;
+	    } else {
+	      float hxy = JointEntropy.calculate(slice, binx, biny);      
+	      float w;
+	      if (Numbers.equal(0.0, hxy)) {
+	        w = 0;
+	      } else {
+	        w = Numbers.constrain(ixy / hxy, 0, 1);
+	        if (Float.isNaN(w)) w = 0;
+	      }
+	      if(w==0)
+	    	  return 0;
+	      else
+	    	  return 1;
+	    }
+	  }  
   
   static public float calculate(DataSlice2D slice, float pvalue, Project prefs) {
     Variable varx = slice.varx;
@@ -53,7 +144,7 @@ public class Similarity {
     } else if (prefs.depTest == DependencyTest.SURROGATE_GENERAL) {      
       indep = surrogateGeneral(slice, ixy, prefs.binAlgo, pvalue);
     } else if (prefs.depTest == DependencyTest.GAMMA_TEST) {
-      indep = gammaTest(ixy, binx, biny, count, pvalue);
+        indep = gammaTest(ixy, binx, biny, count, pvalue);
     }
     
     if (indep) {
@@ -128,5 +219,65 @@ public class Similarity {
     } catch (Exception ex) {
       return true;
     }    
+  }
+  
+  static protected boolean spearmanTest(DataSlice2D slice, double pval, int numTail) {
+	  ArrayList<Value2D> items = slice.values;
+	  int size = items.size();
+	  double [] v1 = new double[size];
+	  double [] v2 = new double[size];
+	  for(int i = 0; i < size; ++i)
+	  {
+		  v1[i] = items.get(i).x;
+		  v2[i] = items.get(i).y;
+	  }
+	    try {
+	    	Double s = new SpearmansCorrelation().correlation(v1,v2);
+	  	  double t = s * Math.sqrt((size - 2) / (1 - s * s));
+	  	  double p = 1 - new TDistribution(size - 1).cumulativeProbability(t);
+	  	  if (numTail == 1)
+	  		  return pval < p;
+	  	  else
+	  		  return pval < 2 * p;	 
+	      } catch (Exception ex) {
+	        return true;
+	      }    
+  }
+
+  static protected boolean pearsonTest(DataSlice2D slice, double pval, int numTail) {
+	  ArrayList<Value2D> items = slice.values;
+	  int size = items.size();
+	  double [] v1 = new double[size];
+	  double [] v2 = new double[size];
+	  for(int i = 0; i < size; ++i)
+	  {
+		  v1[i] = items.get(i).x;
+		  v2[i] = items.get(i).y;
+	  }
+	  double s = new PearsonsCorrelation().correlation(v1,v2);
+	  double t = s * Math.sqrt((size - 2) / (1 - s * s));
+	  double p = 1 - new TDistribution(size - 1).cumulativeProbability(t);
+	  if (numTail == 1)
+		  return pval < p;
+	  else
+		  return pval < 2 * p;	  
+//	  double z = 0.5 * Math.log((1+s)/(1-s));
+//	  double p = 0.5*(1+Erf.erf(z/Math.sqrt(2.0)));
+  }
+  
+  static protected boolean fisherTest(DataSlice2D slice, float pvalue, int binAlgo, int numTail) {
+	  int [][] freqMatrix = Histogram.calculate2DArray(slice, binAlgo);
+
+	try
+	{
+		  double p = hdMM.getHypergeometricDistribution(freqMatrix, 0, RoundingMode.HALF_UP).doubleValue();
+		  if (numTail == 1)
+			  return pvalue < p;
+		  else
+			  return pvalue < p*2;
+	  }
+	  catch(Exception ex) {
+	  	return true;
+	  }
   }
 }
